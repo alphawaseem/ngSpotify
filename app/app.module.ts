eb31f716015bf2ca1f7e66127e73b24da78d9bc0
Waseem Ahmed <alphawaseem@gmail.com>
2017-01-03 16:18:57 +0530

    setup navbar and router

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavBarComponent } from './components/navbar/navbar.component';
 import { AppComponent }  from './app.component';
+import { RouterModule,Routes }   from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
+
+
+const routes : Routes = [
+  {
+        path: '',
+        redirectTo: 'home',
+        pathMatch : 'full' 
+      },
+      {
+        path: 'home',
+        component: SearchComponent
+      },{
+        path : 'about',
+        component : AboutComponent
+      }
+]
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent,NavBarComponent,SearchComponent,AboutComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core';
+
+class NavItem{
+    constructor(public name:string,public url:string){}
+}
+
+@Component({
+    moduleId : module.id,
+    selector : 'navbar',
+    templateUrl : 'navbar.component.html'
+})
+export class NavBarComponent {
+    projectName : string;
+    items : NavItem[] = [];
+    selectedItem : NavItem;
+    constructor(){
+        this.projectName = "ngSpotify";
+        this.items.push(new NavItem('Home','/home'));
+        this.items.push(new NavItem('About','/about'));
+        this.selectedItem=this.items[0];
+    }
+
+    selectItem(item:NavItem):boolean{
+        this.selectedItem = item;
+        return false;
+    }
+
+}
